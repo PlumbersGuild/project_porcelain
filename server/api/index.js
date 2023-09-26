0e6ac05e3ff8e58276c99ac399138f1c4a547fe0
Calvin Thang <calviethang@Calvins-MacBook-Pro.local>
2023-09-26 15:11:53 -0400

    added book routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,1 +1,6 @@
-const express = require('express');
+const express = require("express");
+const router = express.Router();
+
+router.use("/books", require("./books"));
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT =8080;
+const PORT = 8080;
 
-
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(
+	"/",
+	express.static(path.join(__dirname, "public"))
+);
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api", require('./api'))
+app.use("/api", require("./api"));
 // app.use("/auth", require("./auth"))
 
-
-const server = app.listen(PORT, ()=>{
-    console.log('On port'+PORT)
-})
+const server = app.listen(PORT, () => {
+	console.log("On port" + PORT);
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || "Internal server error.");
+	console.error(err.stack);
+	res
+		.status(err.status || 500)
+		.send(err.message || "Internal server error.");
 });
