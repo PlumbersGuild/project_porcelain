f4f4c84d3cf57233148b9e2d2cbe36a1d3a991ac
Garrett Gorman <garrettgorman1@gmail.com>
2023-09-26 10:58:44 -0400

    added auth and api folders. Set up server/index.js

--- server/index.js
+++ server/index.js
@@ -1,2 +1,26 @@
 const express = require("express");
+const app = express();
+const path = require("path");
+const PORT =8080;
 
+
+const cors = require('cors');
+app.use(cors());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api", require('./api'))
+// app.use("/auth", require("./auth"))
+
+
+const server = app.listen(PORT, ()=>{
+    console.log('On port'+PORT)
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+});
