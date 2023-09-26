4d13d893ececf41b457554cfa7bc693e3bed2689
Garrett Gorman <garrettgorman1@gmail.com>
2023-09-26 15:06:42 -0400

    created auth routes, modified package.json

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT =8080;
+const jwt = require("jsonwebtoken");
 
 
 const cors = require('cors');
@@ -12,11 +13,31 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/auth", require("./auth"))
+
+app.use((req, res, next) => {
+    const token = req.headers.authorization;
+    console.log(token)
+    if(!token) {
+        next()
+        return
+    }
+    try {
+        const user = jwt.verify(token, process.env.JWT);
+        req.user = user;
+        next()
+        return
+    } catch (error) {
+        console.error(error);
+        res.status(400).send({message: "Invalid Token"})
+        return 
+    }
+})
 // app.use("/api", require('./api'))
-// app.use("/auth", require("./auth"))
 
 
-const server = app.listen(PORT, ()=>{
+
+app.listen(PORT, ()=>{
     console.log('On port'+PORT)
 })
 
