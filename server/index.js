6b6865b1fff392200f40ef4a733e0111e066ba59
Garrett Gorman <garrettgorman1@gmail.com>
2023-09-27 19:29:02 -0400

    created store.js file, added api reducer, made jsx page for all books

--- server/index.js
+++ server/index.js
@@ -7,10 +7,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 app.use(cors());
 
-app.use(
-	"/",
-	express.static(path.join(__dirname, "public"))
-);
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,32 +15,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", require("./auth"));
 
 app.use((req, res, next) => {
-	const token = req.headers.authorization;
-	console.log(token);
-	if (!token) {
-		next();
-		return;
-	}
-	try {
-		const user = jwt.verify(token, process.env.JWT);
-		req.user = user;
-		next();
-		return;
-	} catch (error) {
-		console.error(error);
-		res.status(400).send({ message: "Invalid Token" });
-		return;
-	}
+  const token = req.headers.authorization;
+  console.log(token);
+  if (!token) {
+    next();
+    return;
+  }
+  try {
+    const user = jwt.verify(token, process.env.JWT);
+    req.user = user;
+    next();
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(400).send({ message: "Invalid Token" });
+    return;
+  }
 });
-// app.use("/api", require('./api'))
+app.use("/api", require("./api"));
 
 app.listen(PORT, () => {
-	console.log("On port" + PORT);
+  console.log("On port" + PORT);
 });
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res
-		.status(err.status || 500)
-		.send(err.message || "Internal server error.");
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
