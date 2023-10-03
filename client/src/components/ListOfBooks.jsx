3da892eb89bdad78f4bc496a6e9bd061a7838206
Garrett Gorman <garrettgorman1@gmail.com>
2023-10-03 12:25:37 -0400

    styled all books and added links to single books

--- client/src/components/ListOfBooks.jsx
+++ client/src/components/ListOfBooks.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 
 function ListOfBooks({ books }) {
-	const filteredBooks = books.filter(
-		(book) => book.price !== "$0.00"
-	);
+  const filteredBooks = books.filter((book) => book.price !== "$0.00");
 
-	return (
-		<div className="list__container">
-			<div className="list__books">
-				{filteredBooks.map((book) => (
-					<div
-						className="list__book"
-						key={book.isbn13}>
-						<h3>{book.title}</h3>
-						<h4>{book.price}</h4>
-						<img
-							src={book.image}
-							alt=""
-						/>
-						<button>Add to Cart</button>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="list__container">
+      <div className="list__books">
+        {filteredBooks.map((book) => (
+          <div className="list__book" key={book.isbn13}>
+            <h3>{book.title}</h3>
+            <h4>$ {book.price / 100}</h4>
+            <img src={book.image} alt="" />
+            <button>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ListOfBooks;
