69de691e0fcd8340a9678729ab67f1874eac925a
Garrett Gorman <garrettgorman1@gmail.com>
2023-10-03 23:46:31 -0400

    added placeholder images and roughly styled single book

--- client/src/pages/SingleBookPage.jsx
+++ client/src/pages/SingleBookPage.jsx
@@ -1,5 +1,6 @@
 import { useGetBookByIdQuery } from "../reducers/api";
 import { useParams } from "react-router-dom";
+import Placeholder from "../assets/placeholder.png";
 
 const SingleBookPage = () => {
   const { id } = useParams();
@@ -26,11 +27,20 @@ const SingleBookPage = () => {
     // </div>
     <div className="main__container">
       <div className="img__container">
-        <img src={book.image} alt={book.title} />
+        <img src={Placeholder} alt={book.title} />
       </div>
-      <div className="information__containers">
-        <h2>{book.title}</h2>
-        <h3>{book.subtitle}</h3>
+      <div className="information__container">
+        <div className="title__container">
+          <h2>{book.title}</h2>
+          <h3>{book.subtitle}</h3>
+        </div>
+        <div className="lorem">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          nesciunt officiis nulla ad nobis! Delectus repudiandae, quibusdam,
+          vero laborum odit atque temporibus unde ullam minus mollitia
+          voluptatem est ipsum nisi.
+        </div>
+        <h4>$ {book.price / 100}</h4>
       </div>
     </div>
   );
