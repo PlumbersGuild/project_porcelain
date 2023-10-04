3f7453035ded47558356b655b6650538cf20a89c
Shaheer-Malik <shaheerm1223@gmail.com>
2023-10-04 14:53:53 -0400

    still working

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,7 +1,17 @@
 function AboutPage() {
 	return (
 		<>
-			<h1>Welcome to the About Page</h1>
+			<h1 className="title">2307-FSA-ET-WEB-FT-SF</h1>
+			
+			<h1 className="divider">________________________________________________________________________________________</h1>
+			
+			<h1 className="description">Amazoni is an innovative and user-friendly E-commerce website that provides an exceptional online shopping experience for customers with interests and preferences for programming. With a sleek and modern design, the website offers a wide range of book products that users can read to enhance their coding abilities.</h1>
+			
+			<h1 className="divider">________________________________________________________________________________________</h1>
+			
+			<h1 className="names">Calvin Thang || Garrett Gorman || Shaheer Malik</h1>
+			
+			<h1 className="divider">________________________________________________________________________________________</h1>
 		</>
 	);
 }
