511860f9b506cab75c2133422038de1c5ea64b4b
Garrett Gorman <garrettgorman1@gmail.com>
2023-10-04 17:09:03 -0400

    added a persistant cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,222 +4,221 @@ const { PrismaClient } = require("@prisma/client");
 const prismaClient = new PrismaClient();
 const verify = require("../auth/verify");
 
-router.get("/", async (req, res, next) => {
-	const { userId } = req.user;
+router.get("/", verify, async (req, res, next) => {
+  const { userId } = req.user;
 
-	if (!userId) {
-		const foundProduct =
-			await prismaClient.product.findFirst({
-				where: {
-					id: productId,
-				},
-			});
-	}
+  //   if (!userId) {
+  //     const foundProduct = await prismaClient.product.findFirst({
+  //       where: {
+  //         id: productId,
+  //       },
+  //     });
+  //   }
 
-	try {
-		const foundOrder = await prismaClient.Order.findFirst({
-			where: {
-				id: userId,
-			},
-		});
+  try {
+    const foundOrder = await prismaClient.order.findFirst({
+      where: {
+        userId: userId,
+        isFulfilled: false,
+      },
+      include: {
+        CartItem: true,
+      },
+    });
 
-		if (foundOrder) {
-			res.status(200).json(foundOrder);
-		}
-	} catch (error) {
-		console.error(error.message);
-		next(error);
-	}
+    if (foundOrder) {
+      console.log("testOrder", foundOrder.CartItem);
+      res.status(200).json(foundOrder.CartItem);
+    }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
 });
 
 router.post("/new", verify, async (req, res, next) => {
-	const { book, qty } = req.body;
-	const { userId } = req.user;
+  const { book, qty } = req.body;
+  const { userId } = req.user;
 
-	console.log(`\n [DEBUG] book: `, req.body);
+  console.log(`\n [DEBUG] book: `, req.body);
 
-	console.log(req.user);
+  console.log(req.user);
 
-	// if (!userId) {
-	// 	const foundProduct =
-	// 		await prismaClient.product.findFirst({
-	// 			where: {
-	// 				id: productId,
-	// 			},
-	// 		});
-	// }
+  // if (!userId) {
+  // 	const foundProduct =
+  // 		await prismaClient.product.findFirst({
+  // 			where: {
+  // 				id: productId,
+  // 			},
+  // 		});
+  // }
 
-	try {
-		// product -> cartItem -> order
-		const foundOrder = await prismaClient.Order.findFirst({
-			where: {
-				id: userId,
-				isFulfilled: false,
-			},
-		});
+  try {
+    // product -> cartItem -> order
+    const foundOrder = await prismaClient.Order.findFirst({
+      where: {
+        userId: userId,
+        isFulfilled: false,
+      },
+    });
 
-		console.log(`\n [DEBUG] foundOrder: `, foundOrder);
+    console.log(`\n [DEBUG] foundOrder: `, foundOrder);
 
-		if (foundOrder) {
-			const foundProduct =
-				await prismaClient.product.findUnique({
-					where: {
-						id: book.id,
-					},
-				});
+    if (foundOrder) {
+      const foundProduct = await prismaClient.product.findUnique({
+        where: {
+          id: book.id,
+        },
+      });
 
-			console.log(
-				`\n [DEBUG] foundProduct: `,
-				foundProduct
-			);
+      console.log(`\n [DEBUG] foundProduct: `, foundProduct);
 
-			if (!foundProduct) {
-				res.status(404).json({
-					message: "Could not find product",
-				});
-			}
+      if (!foundProduct) {
+        res.status(404).json({
+          message: "Could not find product",
+        });
+      }
 
-			const duplicateCartItem =
-				await prismaClient.cartItem.findUnique({
-					where: {
-						orderId_productId: {
-							orderId: foundOrder.id,
-							productId: foundProduct.id,
-						},
-					},
-				});
+      const duplicateCartItem = await prismaClient.cartItem.findUnique({
+        where: {
+          orderId_productId: {
+            orderId: foundOrder.id,
+            productId: foundProduct.id,
+          },
+        },
+      });
 
-			if (duplicateCartItem) {
-				const newQty = duplicateCartItem.qty + qty;
-				await prismaClient.cartItem.update({
-					where: {
-						orderId_productId: {
-							orderId: foundOrder.id,
-							productId: foundProduct.id,
-						},
-					},
-					data: {
-						qty: newQty,
-					},
-				});
-				res.status(200).json(duplicateCartItem);
-			} else {
-				const createdCartItem =
-					await prismaClient.cartItem.create({
-						data: {
-							orderId: foundOrder.id,
-							qty,
-							price: book.price,
-							productId: foundProduct.id,
-						},
-					});
-				res.status(201).json(createdCartItem);
-			}
-		} else {
-			const createdOrder = prismaClient.Order.create({
-				userId,
-			});
+      if (duplicateCartItem) {
+        const newQty = duplicateCartItem.qty + qty;
+        const updated = await prismaClient.cartItem.update({
+          where: {
+            orderId_productId: {
+              orderId: foundOrder.id,
+              productId: foundProduct.id,
+            },
+          },
+          data: {
+            qty: newQty,
+          },
+        });
+        const order = await prismaClient.order.findFirst({
+          where: {
+            userId: userId,
+            isFulfilled: false,
+          },
+          include: {
+            CartItem: true,
+          },
+        });
+        res.status(200).json(order.CartItem);
+      } else {
+        const createdCartItem = await prismaClient.cartItem.create({
+          data: {
+            orderId: foundOrder.id,
+            qty,
+            price: book.price,
+            productId: foundProduct.id,
+          },
+        });
+        res.status(201).json(createdCartItem);
+      }
+    } else {
+      const createdOrder = prismaClient.Order.create({
+        userId,
+      });
 
-			console.log(
-				`\n [DEBUG] createdOrder: `,
-				createdOrder
-			);
-		}
-	} catch (error) {
-		console.error(error.message);
-		next(error);
-	}
+      console.log(`\n [DEBUG] createdOrder: `, createdOrder);
+    }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
 });
 
 // UPDATE productId to book and adjust accordingly
 router.put("/update", async (req, res, next) => {
-	const { productId, qty } = req.body;
-	const { userId } = req.user;
+  const { productId, qty } = req.body;
+  const { userId } = req.user;
 
-	try {
-		const foundOrder = await prismaClient.Order.findFirst({
-			where: {
-				userId: userId,
-				isFulfilled: false,
-			},
-		});
+  try {
+    const foundOrder = await prismaClient.Order.findFirst({
+      where: {
+        userId: userId,
+        isFulfilled: false,
+      },
+    });
 
-		if (foundOrder) {
-			const updateCartItem =
-				await prismaClient.cartItem.update({
-					where: {
-						orderId_productId: {
-							orderId: foundOrder.id,
-							productId,
-						},
-					},
-					data: {
-						qty,
-					},
-				});
+    if (foundOrder) {
+      const updateCartItem = await prismaClient.cartItem.update({
+        where: {
+          orderId_productId: {
+            orderId: foundOrder.id,
+            productId,
+          },
+        },
+        data: {
+          qty,
+        },
+      });
 
-			if (!updateCartItem) {
-				res.status(400).json({
-					message: "Could not update cart item",
-				});
-			}
-			res.status(200).json(updateCartItem);
-		} else {
-			res.status(404).json({
-				message: "Could not find order",
-			});
-		}
-	} catch (error) {
-		console.error(error.message);
-		next(error);
-	}
+      if (!updateCartItem) {
+        res.status(400).json({
+          message: "Could not update cart item",
+        });
+      }
+      res.status(200).json(updateCartItem);
+    } else {
+      res.status(404).json({
+        message: "Could not find order",
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
 });
 
 router.delete("/delete", async (req, res, next) => {
-	const { productId } = req.body;
-	const { userId } = req.user;
+  const { productId } = req.body;
+  const { userId } = req.user;
 
-	try {
-		const foundOrder = await prismaClient.Order.findFirst({
-			where: {
-				userId: userId,
-				isFulfilled: false,
-			},
-		});
+  try {
+    const foundOrder = await prismaClient.Order.findFirst({
+      where: {
+        userId: userId,
+        isFulfilled: false,
+      },
+    });
 
-		if (!foundOrder) {
-			res.status(400).json({
-				message: "Cannot delete b/c cannot locate order",
-			});
-		}
+    if (!foundOrder) {
+      res.status(400).json({
+        message: "Cannot delete b/c cannot locate order",
+      });
+    }
 
-		const foundCartItem =
-			await prismaClient.cartItem.findFirst({
-				where: {
-					productId,
-				},
-			});
+    const foundCartItem = await prismaClient.cartItem.findFirst({
+      where: {
+        productId,
+      },
+    });
 
-		const deletedCartItem =
-			await prismaClient.cartItem.delete({
-				where: {
-					orderId_productId: {
-						orderId: foundCartItem.orderId,
-						productId: foundCartItem.productId,
-					},
-				},
-			});
+    const deletedCartItem = await prismaClient.cartItem.delete({
+      where: {
+        orderId_productId: {
+          orderId: foundCartItem.orderId,
+          productId: foundCartItem.productId,
+        },
+      },
+    });
 
-		if (!deletedCartItem) {
-			res
-				.status(400)
-				.json({ message: "Could not delete cart item" });
-		}
-		res.status(200).json(deletedCartItem);
-	} catch (error) {
-		console.error(error.message);
-		next(error);
-	}
+    if (!deletedCartItem) {
+      res.status(400).json({ message: "Could not delete cart item" });
+    }
+    res.status(200).json(deletedCartItem);
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
 });
 
 module.exports = router;
